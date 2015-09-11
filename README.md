da5a64d4e0f36d16b0e7f472a99a3f2b169119d0
Winston Kim <winzton.kim@gmail.com>
2015-09-10 17:09:23 -0700

    SISRP-6547 - Campus Solutions splash page

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # The SIS Splash Page
-This is the splash page for PeopleSoft consultants to log in to Campus Solutions. Consultants can access the log in portal through the splash page in addition to links to useful resources that a consultant might need.
+This is the splash page for PeopleSoft consultants, staff, and faculty to log in to Campus Solutions. Users can access the log in portal through the splash page in addition to links to useful resources that a consultant might need.
 ## Build
 This page uses Gulp to convert the SCSS files into inline CSS. Run the following command to compile the splash page:
 
--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,12 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var inlinesource = require('gulp-inline-source');
 var autoprefixer = require('gulp-autoprefixer');
-
-// Lint Task
-gulp.task('lint', function() {
-  return gulp.src('js/*.js')
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'));
-});
 
 // Compile Our Sass
 gulp.task('sass', function() {
@@ -28,16 +20,6 @@ gulp.task('sass', function() {
   .pipe(gulp.dest('src/css'));
 });
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-  return gulp.src('js/*.js')
-  .pipe(concat('all.js'))
-  .pipe(gulp.dest('dist'))
-  .pipe(rename('all.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('dist'));
-});
-
 gulp.task('inlinesource', ['sass'], function() {
   return gulp.src('./src/index.html')
   .pipe(inlinesource())
@@ -46,9 +28,8 @@ gulp.task('inlinesource', ['sass'], function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-  gulp.watch('js/*.js', ['lint', 'scripts']);
   gulp.watch('src/scss/*.scss', ['inlinesource']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch', 'inlinesource']);
+gulp.task('default', ['sass', 'watch', 'inlinesource']);
